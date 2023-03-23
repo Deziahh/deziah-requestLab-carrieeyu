93acbb5550d3fd55c8d422817d5dc5fc370c0a1a
carrie <cyu1088@gmail.com>
2023-03-22 23:46:22 -0400

    added

---
+++ funcs.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "funcs.h"
+
+//Task A
+
+/*
+Write a new program invert.cpp that inverts all colors, so white shades become black, and black become white:
+
+Since black = 0, and white = 255, you should do the following transformation for each pixel color:
+
+0 → 255
+1 → 254
+2 → 253
+…
+254 → 1
+255 → 0
+*/
